a0d696524cf436455eb190eb64ea0772731fff29
Bowen Zhong <bz4e14@soton.ac.uk>
2017-06-20 18:32:58 +0800

    initial WebApiBase

--- lib_angular/core/WebApiBase.js
+++ lib_angular/core/WebApiBase.js
@@ -1,10 +1,11 @@
 app.factory('WebApiBase', 
   [
     '$log',
-    '$http'
-  ],
+    '$q',
+    '$http',
   function(
     $log,
+    $q,
     $http
   ) {
 
@@ -12,6 +13,57 @@ app.factory('WebApiBase',
 
     };
 
-    
+    angular.extend(WebApiBase.prototype, {
+      
+      // base data which will send to server in every request
+      baseData: {},
 
-  });
+      // parse http response data
+      responseDataParser: function(response) {
+        var result = response.data || {};
+        return {
+          code: result.code || '0000',
+          message: result.message || '',
+          data: result.data ? {} : result.data
+        }
+      },
+
+      // error parse which deal with http response error
+      responseErrorParser: function(response) {
+        var result = response.data || {};
+        return {
+          code: result.code || '9999',
+          message: result.message || 'server error',
+          data: result.data ? {} : result.data
+        }
+      },
+      
+      // gather request data which will be sent to server
+      getRequestData: function(data) {
+        return angular.extend(this.baseData, data);
+      },
+
+      // perform http request
+      request: function(settting, resProcess) {
+        var outerThis = this;
+        var deferred = $q.defer();
+
+        $http(settting)
+          .then(function(result) {
+            result = outerThis.responseDataParser(result);
+            result = angular.isFunction(resProcess) ? resProcess(result) : result;
+            deferred.resolve(result);
+          })
+          .catch(function(error) {
+            error = outerThis.responseErrorParser(error);
+            deferred.reject(error)
+          });
+
+        return deferred.promise;
+      }
+
+    });
+
+    return WebApiBase;
+
+  }]);
--- lib_angular/core/index.js
+++ lib_angular/core/index.js
@@ -1,2 +1,2 @@
-// require('./WebApiBase.js');
+require('./WebApiBase.js');
 require('./ngDialog.js');
