5a07204d64f39107e0a9664b4d7f053b5627a27b
Bowen Zhong <bz4e14@soton.ac.uk>
2017-06-21 15:50:05 +0800

    application.js

---
+++ lib_angular/core/application.js
@@ -0,0 +1,20 @@
+// file which define appplication global funcations
+function getQueryString(queryName) {
+  var reg = new RegExp('(^|&)' + queryName + '=([^&]*)(&|$)', 'i');
+  var matched = window.location.search.substr(1).match(reg);
+
+  if (matched != null) {
+    return decodeURIComponent(matched[2]);
+  }
+  return null;
+}
+
+function getEnvName() {
+  var envName = getQueryString('env');
+  return envName;
+}
+
+window.BLOG = {
+  getQueryString: getQueryString,
+  envName: getEnvName()
+};
--- lib_angular/core/index.js
+++ lib_angular/core/index.js
@@ -1,3 +1,4 @@
 require('./WebApiBase.js');
 require('./ngDialog.js');
 require('./extend.js');
+require('./application.js');
